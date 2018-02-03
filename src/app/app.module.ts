12a079d25dd4718b7b5e342fcc19556ff07133a4
Prashanth Kumar hirematam <prashanthkumarhirematam@gmail.com>
2018-02-03 18:23:21 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { baseURL } from './shared/baseurl';
 import { ProcessHttpmsgService } from './services/process-httpmsg.service';
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -42,6 +43,7 @@ import { RestangularConfigFactory } from './shared/restConfig';
     AboutComponent,
     ContactComponent,
     LoginComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
